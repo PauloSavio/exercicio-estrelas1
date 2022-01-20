38144521b7ffc238698855b04b20a913d1c13dd2
Savio <pauloluizsavio@gmail.com>
2022-01-19 21:41:50 -0300

    Criar kombi

--- src/main/java/br/com/zup/estrelasexercicio1/CarroController.java
+++ src/main/java/br/com/zup/estrelasexercicio1/CarroController.java
@@ -1,16 +1,16 @@
 package br.com.zup.estrelasexercicio1;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 
 @RestController
+@RequestMapping("/carros")
 public class CarroController {
 
-    @GetMapping("/carros/fusca")
+    @GetMapping("/fusca")
     public HashMap<String, String> exibirFusca(){
         HashMap<String, String> fusca = new HashMap<>();
         fusca.put("Cor", "Azul");
@@ -19,4 +19,15 @@ public class CarroController {
 
         return fusca;
     }
+
+    @GetMapping("/kombi")
+    public HashMap<String, String> exibirKombi() {
+        HashMap<String, String> kombi = new HashMap<>();
+        kombi.put("Cor", "Branca");
+        kombi.put("Ano", "1974");
+        kombi.put("Motor", "1600");
+
+        return kombi;
+    }
 }
+
