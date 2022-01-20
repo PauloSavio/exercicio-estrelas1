7ae06a78546038ce1ad45210ee9e43e8d8960a7e
Savio <pauloluizsavio@gmail.com>
2022-01-19 22:59:50 -0300

    Criar carroDTO

--- src/main/java/br/com/zup/estrelasexercicio1/controllers/CarroController.java
+++ src/main/java/br/com/zup/estrelasexercicio1/controllers/CarroController.java
@@ -1,4 +1,4 @@
-package br.com.zup.estrelasexercicio1;
+package br.com.zup.estrelasexercicio1.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
