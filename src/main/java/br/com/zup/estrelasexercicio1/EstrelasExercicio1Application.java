3982ef189289eaad521db5d05c761783d6ff4b19
Savio <pauloluizsavio@gmail.com>
2022-01-19 21:09:54 -0300

    Primeiro commit

---
+++ src/main/java/br/com/zup/estrelasexercicio1/EstrelasExercicio1Application.java
@@ -0,0 +1,13 @@
+package br.com.zup.estrelasexercicio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstrelasExercicio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstrelasExercicio1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/estrelasexercicio1/EstrelasExercicio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.estrelasexercicio1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstrelasExercicio1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
