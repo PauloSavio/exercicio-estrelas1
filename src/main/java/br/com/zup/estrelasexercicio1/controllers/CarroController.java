c3a5187c0f787c76fb878951aad838d40b81f16a
Savio <pauloluizsavio@gmail.com>
2022-01-19 23:13:03 -0300

    Criar lista de carros

--- src/main/java/br/com/zup/estrelasexercicio1/controllers/CarroController.java
+++ src/main/java/br/com/zup/estrelasexercicio1/controllers/CarroController.java
@@ -1,33 +1,18 @@
 package br.com.zup.estrelasexercicio1.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.zup.estrelasexercicio1.dtos.CarroDTO;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/carros")
 public class CarroController {
-
-    @GetMapping("/fusca")
-    public HashMap<String, String> exibirFusca(){
-        HashMap<String, String> fusca = new HashMap<>();
-        fusca.put("Cor", "Azul");
-        fusca.put("Ano", "1964");
-        fusca.put("Motor", "1000");
-
-        return fusca;
-    }
-
-    @GetMapping("/kombi")
-    public HashMap<String, String> exibirKombi() {
-        HashMap<String, String> kombi = new HashMap<>();
-        kombi.put("Cor", "Branca");
-        kombi.put("Ano", "1974");
-        kombi.put("Motor", "1600");
-
-        return kombi;
+    private List<CarroDTO> concessionaria = new ArrayList<>();
     }
 }
 
