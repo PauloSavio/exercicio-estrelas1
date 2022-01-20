b895b01bf38c06c823107e547f050c99b78ed87e
Savio <pauloluizsavio@gmail.com>
2022-01-19 23:27:18 -0300

    Criar metodo para adicionar na lista de carros

--- src/main/java/br/com/zup/estrelasexercicio1/controllers/CarroController.java
+++ src/main/java/br/com/zup/estrelasexercicio1/controllers/CarroController.java
@@ -2,8 +2,7 @@ package br.com.zup.estrelasexercicio1.controllers;
 
 
 import br.com.zup.estrelasexercicio1.dtos.CarroDTO;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +12,15 @@ import java.util.List;
 @RequestMapping("/carros")
 public class CarroController {
     private List<CarroDTO> concessionaria = new ArrayList<>();
+
+    @GetMapping
+    public List<CarroDTO> exibirTodosOsCarros(){
+        return concessionaria;
+    }
+
+    @PostMapping
+    public void cadastrarCarro(@RequestBody CarroDTO carroDTO){
+        concessionaria.add(carroDTO);
     }
 }
 
